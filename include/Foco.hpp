dfadf69c48a06306b0f73dcf0ac609e8a62530dc
Alan23310 <a23310399@live.ceti.mx>
2024-10-24 02:07:11 +0000

    clase 3

--- include/Foco.hpp
+++ include/Foco.hpp
@@ -1,3 +1,10 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+
+
 class Foco
 {
 private:
@@ -12,5 +19,15 @@ public:
     
     void Apagar(){
         this ->encendido = false;
+    }
+
+    void Imprimir(){
+        if(this->encendido){
+            cout<<"O";
+        }
+        else{
+            cout<<".";
+        }
+
     }
 };
--- include/Lienzo.hpp
+++ include/Lienzo.hpp
@@ -1,8 +1,26 @@
+#pragma once
+#include <Vector.hpp>
+#include <SerieNavidad.hpp>
 class Lienzo
 {
 private:
+    Vector posicion;
+    Vector tamano;
     
 public:
-    Lienzo() {}
+    Lienzo(Vector p, Vector t) {
+        this->posicion = p;
+        this->tamano = t;
+
+    }
     ~Lienzo() {}
 
+    void Dibujar(SerieNavidad serie){
+        serie.Imprimir();
+    }
+
+    void Limpiar(){
+        system("clear");
+    }
+
+};
