49134ef05f3cdcf14ea9bdd5bd90c333e3fcddd1
Alan23310 <a23310399@live.ceti.mx>
2024-11-01 01:56:41 +0000

    clase 4

--- src/test.cpp
+++ src/test.cpp
@@ -15,33 +15,55 @@ int main(int argc, char const *argv[])
 
     while (true)
     {
-        auto can = Canvas(300,100);
-        //can.DrawBlockCircle(150,25,frame);
-        can.DrawBlockCircle(150,25,frame+90);
-        can.DrawBlockCircle(150,25,frame+80);
-        can.DrawBlockCircle(150,25,frame+70);
-        can.DrawBlockCircle(150,25,frame+60);
-        can.DrawBlockCircle(150,25,frame+50);
-        can.DrawBlockCircle(150,25,frame+40);
-        can.DrawBlockCircle(150,25,frame+30);
-        can.DrawBlockCircle(150,25,frame+20);
-        can.DrawBlockCircle(150,25,frame+10);
-        can.DrawBlockCircle(150,25,frame);
+        auto can = Canvas(300,300);
         
- /*
-       can.DrawBlockLine(10,10,10,10);
-       can.DrawBlockLine(10,10,15,20);
-       can.DrawBlockLine(10,10,20,30);
-       can.DrawBlockLine(10,10,30,40);
-       can.DrawBlockLine(10,10,40,50);
-       can.DrawBlockLine(10,10,50,60);
-       can.DrawBlockLine(10,10,60,70);
-       can.DrawBlockLine(10,10,70,80);
-  */
+        can.DrawBlockCircle(150,50,frame);
+
+
+        can.DrawBlockCircle(145,45,frame);
+        can.DrawBlockCircle(140,40,frame);
+        can.DrawBlockCircle(135,35,frame);
+        can.DrawBlockCircle(130,30,frame);
+        can.DrawBlockCircle(125,30,frame);
+        can.DrawBlockCircle(120,35,frame);
+        can.DrawBlockCircle(115,40,frame);
+        can.DrawBlockCircle(110,45,frame);
+        can.DrawBlockCircle(105,50,frame);
+        can.DrawBlockCircle(105,55,frame);
+        can.DrawBlockCircle(110,60,frame);
+        can.DrawBlockCircle(115,65,frame);
+        can.DrawBlockCircle(120,70,frame);
+        can.DrawBlockCircle(125,75,frame);
+        can.DrawBlockCircle(130,80,frame);
+        can.DrawBlockCircle(135,85,frame);
+        can.DrawBlockCircle(140,90,frame);
+        can.DrawBlockCircle(145,95,frame);
+
+
+        can.DrawBlockCircle(150,100,frame);
+
 
         
-        
-        //can.DrawBlockLine(10,10,frame,10);
+        can.DrawBlockCircle(155,45,frame);
+        can.DrawBlockCircle(160,40,frame);
+        can.DrawBlockCircle(165,35,frame);
+        can.DrawBlockCircle(170,30,frame);
+        can.DrawBlockCircle(175,30,frame);
+        can.DrawBlockCircle(180,35,frame);
+        can.DrawBlockCircle(185,40,frame);
+        can.DrawBlockCircle(190,45,frame);
+        can.DrawBlockCircle(195,50,frame);
+        can.DrawBlockCircle(195,55,frame);
+        can.DrawBlockCircle(190,60,frame);
+        can.DrawBlockCircle(185,65,frame);
+        can.DrawBlockCircle(180,70,frame);
+        can.DrawBlockCircle(175,75,frame);
+        can.DrawBlockCircle(170,80,frame);
+        can.DrawBlockCircle(165,85,frame);
+        can.DrawBlockCircle(160,90,frame);
+        can.DrawBlockCircle(155,95,frame);
+    
+
         
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
         Element lienzo = bgcolor(Color::Blue, canvas(&can));
@@ -49,7 +71,7 @@ int main(int argc, char const *argv[])
         std::cout << reset_position;
         pantalla.Print();
         reset_position = pantalla.ResetPosition(true);
-        this_thread::sleep_for(0.08s);
+        this_thread::sleep_for(0.3s);
         frame++;
     }
 
